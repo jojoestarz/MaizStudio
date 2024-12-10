a3f98e282d4dd5017555819096d7cc84e55005f0
ethan haastrup <ethanhaastrup@gmail.com>
2024-12-10 20:15:29 +0000

    large screen media query
    improved responsiveness for large screens

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Card from "./components/Map-Card";
 import NavBar from "./components/nav-bar";
 import Treeimg from "./assets/trees.png";
 import sky from "./assets/skyview.png";
+import '@fontsource/league-spartan'; // Defaults to weight 400
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <div className="bodyContainer">
    
         <h1 id="title">Maiz Studio +</h1> 
-        <div id="InfoCard">Weekly updates <p>this is gonna be a carousel of media links, map updates, and dynamic info </p></div>
+        <div id="InfoCard"> <h2>Weekly updates</h2> <p>this is gonna be a carousel of media links, map updates, and dynamic info </p></div>
         <div className="Tycoon-Grid">
         
         <Card img={Treeimg} description="treePic"/>
@@ -20,6 +21,7 @@ function App() {
         <Card img={sky} description="This is a description" /> 
         <Card img={Treeimg} description="This is a description" />
         <Card img={sky} description="This is a description" />
+        <Card img={Treeimg} description="This is a description" />
         </div>
         
 
